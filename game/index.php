ee1d4586975157afb5643bcb82a63878279b3d1e
Bailey Pumfleet <bailey@baiplr.me>
2017-08-18 12:05:59 +0100

    Create Basic Game Functionality

--- game/index.php
+++ game/index.php
@@ -4,6 +4,36 @@
   <meta charset="utf-8">
   <title>Practice | Multipli</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS Files -->
+  <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
+  <link href="style.css" rel="stylesheet" media="screen">
+</head>
+<body>
+  <p><span id="int1"></span> x <span id="int2"></span></p>
+  <form>
+      Answer: <input type="text" name="answer" id="answer" autocomplete="off" autofocus><br>
+      <input type="submit" value="Submit" onclick="checkAnswer()">
+  </form>
+
+  <!-- Game Script -->
+  <script type="text/javascript">
+
+    var int1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    var int2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+    var correctAnswer = int1 * int2;
+
+    document.getElementById("int1").innerHTML = int1;
+    document.getElementById("int2").innerHTML = int2;
+
+    function checkAnswer() {
+      if(document.getElementById('answer').value == correctAnswer) {
+        alert("Correct Answer");
+      } else {
+        alert("Incorrect Answer");
+      }
+    }
+  </script>
   <!-- Bootstrap -->
   <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
 </head>
